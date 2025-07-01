63f79d048feba460cd9af80de5baedfcd198a331
StoryOnJava <jagzapsagar228@gmail.com>
2025-07-01 10:16:24 +0530

    cross origin added

--- VRMBookingService/src/main/java/com/example/VRMBookingService/controller/BookingController.java
+++ VRMBookingService/src/main/java/com/example/VRMBookingService/controller/BookingController.java
@@ -3,6 +3,7 @@ package com.example.VRMBookingService.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/booking")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BookingController {
 	
 	@Autowired
--- VRMSecurityService/src/main/java/com/example/VRMSecurityService/controller/SecurityController.java
+++ VRMSecurityService/src/main/java/com/example/VRMSecurityService/controller/SecurityController.java
@@ -3,6 +3,7 @@ package com.example.VRMSecurityService.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import com.example.VRMSecurityService.util.JwtUtil;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SecurityController {
 	@Autowired
     private RestTemplate restTemplate;
